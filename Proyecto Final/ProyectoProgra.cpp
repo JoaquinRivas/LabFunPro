b28797220f3fff2ad4d9db4dd31091258e57f88a
rusanrod <rsandovr@gmail.com>
2017-05-12 11:39:29 -0500

    Update ProyectoProgra.cpp

--- Proyecto Final/ProyectoProgra.cpp
+++ Proyecto Final/ProyectoProgra.cpp
@@ -1,46 +1,54 @@
-//-------Proyecto final de la materia "Fundamentos de programaciÛn"-------
-//-------Se realizÛ por:					  -------
-//-------*GÛmez Arizmendi JosÈ		 		  -------
-//-------*MartÌnez Rosete Karol Joshua		  -------
-//-------*Rivas Vel·zquez JosÈ JoaquÌn		  -------
-//-------*Sandoval RodrÌguez RubÈn Ulises	  -------
+//-------Proyecto final de la materia "Fundamentos de programaci√≥n"-------
+//-------Se realiz√≥ por:					  -------
+//-------*G√≥mez Arizmendi Jos√©		 		  -------
+//-------*Mart√≠nez Rosete Karol Joshua		  -------
+//-------*Rivas Vel√°zquez Jos√© Joaqu√≠n		  -------
+//-------*Sandoval Rodr√≠guez Rub√©n Ulises	  -------
 //-------*Trinidad Barbero Jorge A. 		  -------
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <windows.h>
+//seccion de funciones 
+void gotoxy(int x,int y)
+{
+	COORD coord;
+	coord.X=x;
+	coord.Y=y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+void presentacion(void)
+	{//				Aqui se realiza la funci√≥n que ejecuta la presentaci√≥n del programa
+		gotoxy(25,2);
+		printf("FACULTAD DE INGENIER%cA \n Proyecto realizado por:\n G%cmez Arizmendi Jos%c Luis \n Mart%cnez Rosete Karol Joshua \n",214,162,130,161);
+		printf(" Rivas Vel%czquez Jos%c Joaqu%cn \n Sandoval Rodr%cguez Ruben Ulises \n Trinidad Barbero Jorge Alfredo \n",160,130,161);
+		printf(" Para la materia de: Fundamentos de programaci%cn \n Profesora:Cintia Quezada Reyez \n Grupo: 01 \n",162);
+	}
 int main(){
 //Apuntuntadores para el manejo de archivos
 	FILE *entrada;  //origen
 	FILE *salida;  //Destino
 //variables a utilizar
 	char nombreUsuario[]=" ";		//Apuntador para el nombre del usuario
-	char nombreArchivoIn[]=" ";			//Apuntador para el nombre que se le dar· al archivo de entrada
+	char nombreArchivoIn[]=" ";			//Apuntador para el nombre que se le dar√° al archivo de entrada
 	char nombreArchivoOut[]=" ";		//Apuntador para el nombre del archivo de salida
 	char matriz [6][6] = {{NULL,'1','1','1','1','1'},{'1','A','B','C','D','E'},{'1','F','G','H','I','K'},{'1','L','M','N','O','P'},{'1','Q','R','S','T','U'},{'1','V','W','X','Y','Z'}};//Arreglo bidimensional para la matriz de transformaciones
-	char menu;						//Variable para la elecciÛn en el men˙
+	char menu;						//Variable para la elecci√≥n en el men√∫
 	char string[]=" ";				//variable para lectura del archivo de entrada
-	char letra;						//Variable para asignaciÛn de las letras que introduce el usuario
+	char letra;						//Variable para asignaci√≥n de las letras que introduce el usuario
 	char escritura []=" ";			//Variable para cadena de escritura en archivo de salida
 	//Variables auxiliares
 	int o = 0;						
 	int l = 0;
 	int u = 0;
-//P·gina de presentaciÛn
-	puts("Proyecto realizado por:\n");
-	puts("Gomez Arizmendi Jose\n");
-	puts("Martinez Rosete Karol Joshua\n");
-	puts("Rivas Velazquez Jose Joaquin\n");
-	puts("Sandoval Rodriguez Ruben Ulises\n");
-	puts("Trinidad Barbero Jorge A.\n");
-	puts("Para la materia de: Fundamentos de programacion\n");
-	puts("Grupo: 01		Profesora: M.C. Cintia Quezada Reyes\n");
+//P√°gina de presentaci√≥n
+	presentacion();
 	getchar();
 	//getchar();
 	system("cls"); 			//limpiar pantalla
-//P·gina de datos del usuario	
+//P√°gina de datos del usuario	
 	puts("Por favor, ingresa tu nombre:\n");
 	fflush(stdin);
     scanf("%[^\n]s",nombreUsuario);
@@ -62,7 +70,7 @@ int main(){
     fflush(stdin);
    	scanf("%[^\n]s",nombreArchivoOut);
 	system("cls"); 			//limpiar pantalla
-//P·gina de men˙ y repeticiÛn
+//P√°gina de men√∫ y repetici√≥n
 	while(1){
 		puts("Selecciona una opcion y presiona enter");
 		puts("a) Modificacion del texto");
@@ -81,7 +89,7 @@ int main(){
 							matriz[0][i] = letra;
 							matriz[i][0] = letra;}
 						else{
-							puts("su letra no corresponde al alfabeto inglÈs");	
+							puts("su letra no corresponde al alfabeto ingl√©s");	
 							}
 									}
 					do{
