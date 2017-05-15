9a7e5570295edecf7b2e5abd81d4d9b510eacbfb
rusanrod <rsandovr@gmail.com>
2017-05-14 21:32:10 -0500

    Update ProyectoProgra.cpp

--- Proyecto Final/ProyectoProgra.cpp
+++ Proyecto Final/ProyectoProgra.cpp
@@ -12,6 +12,9 @@
 #include <ctype.h>
 #include <windows.h>
 #define p printf 
+char matriz [7][7] = {{' ',' ',' ',' ',' ',' '},{' ','A','B','C','D','E'},{' ','F','G','H','I','K'},{' ','L','M','N','O','P'},{' ','Q','R','S','T','U'},{' ','V','W','X','Y','Z'}};
+char cadena[25];		//Arreglo para lectura del archivo de entrada sin espacio
+char frase[25];			//Arreglo para lectura del archivo de entrada dado por el usuario
 //seccion de funciones 
 void gotoxy(int x,int y)
 {
@@ -21,40 +24,56 @@ void gotoxy(int x,int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
 void presentacion(void)//				Aqui se realiza la función que ejecuta la presentación del programa
-	{
-		gotoxy(25,2);
-		p("FACULTAD DE INGENIER%cA \n Proyecto realizado por:\n G%cmez Arizmendi Jos%c Luis \n Mart%cnez Rosete Karol Joshua \n",214,162,130,161);
-		p(" Rivas Vel%czquez Jos%c Joaqu%cn \n Sandoval Rodr%cguez Ruben Ulises \n Trinidad Barbero Jorge Alfredo \n",160,130,161);
-		p(" Para la materia de: Fundamentos de programaci%cn \n Profesora:Cintia Quezada Reyez \n Grupo: 01 \n",162);
-	}
+{
+	gotoxy(25,2);
+	p("FACULTAD DE INGENIER%cA \n Proyecto realizado por:\n G%cmez Arizmendi Jos%c Luis \n Mart%cnez Rosete Karol Joshua \n",214,162,130,161);
+	p(" Rivas Vel%czquez Jos%c Joaqu%cn \n Sandoval Rodr%cguez Ruben Ulises \n Trinidad Barbero Jorge Alfredo \n",160,130,161);
+	p(" Para la materia de: Fundamentos de programaci%cn \n Profesora:Cintia Quezada Reyez \n Grupo: 01 \n",162);
+}
 char extraerEspacios(char cadena[25])
-	{
-	char cadena2[25];
+{
 	int tamcad,i,e;
-	tamcad=strlen(cadena);
+	p("Da la frase");
+	fflush(stdin);
+	gets(frase);
+	tamcad=strlen(frase);
 	e=0;
 	for (i=0;i<=tamcad;i++)
-		{
+	{
 		if(cadena[i]!=' ')
-			{
-			cadena2[e]=cadena[i];
-			e=e+1;
-			}
-					
-		}
-int main(){
+		{
+			cadena[e]=frase[i];
+			e=e+1;			
+		}		
+	}			
+}
+void asignacion(char let[6])
+{
+	int col, ren;
+	int i=0;
+	for(col=1;col<=6;col++)
+	{
+		matriz[0][col]=let[i];
+		i=i+1;
+	}
+	i=0;	
+	for(ren=1;ren<=6;ren++)
+	{
+		matriz[ren][0]=let[i];
+		i=i+1;
+	}
+}
+int main()
+{
 	//Apuntuntadores para el manejo de archivos
 	FILE *entrada;  //origen
 	FILE *salida;  //Destino
-	
 	//variables a utilizar
 	char nombreUsuario[25];		//Arreglo para el nombre del usuario
 	char nombreArchivoIn[16];	//Arreglo para el nombre que se le dará al archivo de entrada
 	char nombreArchivoOut[16];	//Arreglo para el nombre del archivo de salida
-	char string[]=" ";		//Arreglo para lectura del archivo de entrada
-	char matriz [6][6] = {{NULL,'1','1','1','1','1'},{'1','A','B','C','D','E'},{'1','F','G','H','I','K'},{'1','L','M','N','O','P'},{'1','Q','R','S','T','U'},{'1','V','W','X','Y','Z'}};//Arreglo bidimensional para la matriz de transformaciones
 	char menu;			//Variable para la selección en el menú
-	char letra;			//Variable para asignación de las letras que introduce el usuario
+	char letras5[6];			//Variable para asignación de las letras que introduce el usuario
 	char escritura []=" ";		//Variable para cadena de escritura en archivo de salida
 	char ext[5]=".txt";		//para posteriormente agregar extensión
 	//Variables auxiliares
@@ -66,7 +85,6 @@ int main(){
 	presentacion();
 	getchar();			//getchar();
 	system("cls"); 			//limpiar pantalla
-	
 	//Página de datos del usuario	
 	p("Por favor, ingrese su nombre (m%cximo 25 caracteres), al finalizar, presione enter \n",160);
 	fflush(stdin);
@@ -75,9 +93,9 @@ int main(){
     	fflush(stdin);
     	gets(nombreArchivoIn);
 	strcat(nombreArchivoIn, ext);
-    	entrada = fopen (nombreArchivoIn , "r");
+    	entrada = fopen (nombreArchivoIn , "w");
     	if (entrada == NULL) 
-			p("Error abriendo archivo");
+		p("Error abriendo archivo");
    	else{
     	     fgets (string , 1000 , entrada) != NULL;
 	     fclose(entrada);
@@ -91,7 +109,6 @@ int main(){
    	gets(nombreArchivoOut);
 	strcat(nombreArchivoOut, ext);
 	system("cls"); 			//limpiar pantalla
-	
 	//Página de menú y repetición del mismo
 	do{
 	   p("Selecciona una opcion y presiona enter\n");
